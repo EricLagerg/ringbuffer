cbe056183a7059f07f8af834617e13fe2378898d
Eric Lagergren <ericscottlagergren@gmail.com>
2016-03-14 01:03:17 -0700

    minor updates

---
+++ doc.go
@@ -0,0 +1,6 @@
+// Copyright 2015 Eric Lagergren.
+// Use of this source code is governed by the MIT
+// license that can be found in the LICENSE file.
+
+// Package ring provides a simple ring buffer implementation.
+package ring
--- rb.go
+++ rb.go
@@ -1,68 +1,81 @@
-// Package ring provides a simple ring buffer implementation.
+// Copyright 2015 Eric Lagergren.
+// Use of this source code is governed by the MIT
+// license that can be found in the LICENSE file.
+
 package ring
 
+// Buffer is a ring buffer.
 type Buffer struct {
-	Data    []int
-	Default int
-	Front   int
-	Back    int
-	Empty   bool
+	data  []int
+	front int
+	back  int
 }
 
-// NewBuffer returns a pointer to a Buffer with the given size
+// NewBuffer returns a Buffer with the given size - 1
 // filled with defaultVal.
 func NewBuffer(defaultVal, size int) *Buffer {
+	data := make([]int, size)
 
-	ir := &Buffer{
-		Data:    make([]int, size),
-		Default: defaultVal,
-		Empty:   true,
+	// Go automatically zeros memory.
+	if defaultVal != 0 {
+		for i := range data {
+			data[i] = defaultVal
+		}
 	}
-
-	for i := 0; i < size; i++ {
-		ir.Data[i] = defaultVal
-	}
-
-	return ir
+	return &Buffer{data: data}
 }
 
-// IsEmpty returns true if the Buffer is empty.
-func (b *Buffer) IsEmpty() bool { return b.Empty }
+// Empty returns true if the Buffer is empty.
+func (b *Buffer) Empty() bool {
+	return b.front == b.back
+}
+
+// Full returns true if the Buffer is full.
+func (b *Buffer) Full() bool {
+	return b.front == (b.front+len(b.data)-1)%len(b.data)
+}
+
+// Clear clears the Buffer.
+func (b *Buffer) Clear() {
+	b.front = 0
+	b.back = 0
+}
 
 // Push pushes a new int into the Buffer.
-func (b *Buffer) Push(x int) int {
+func (b *Buffer) Push(x int) (old int) {
 	empty := 1
-	if b.Empty {
+	if b.Empty() {
 		empty = 0
 	}
 
-	idx := (b.Front + empty) % len(b.Data)
-	old := b.Data[idx]
-	b.Data[idx] = x
-	b.Front = idx
-
-	if idx == b.Back {
-		b.Back = (b.Back + empty) % len(b.Data)
+	idx := (b.front + empty) % len(b.data)
+	old = b.data[idx]
+	b.data[idx] = x
+	b.front = idx
+	if idx == b.back {
+		b.back = (b.back + empty) % len(b.data)
 	}
-	b.Empty = false
-
 	return old
 }
 
 // Pop removes an int from the Buffer.
-func (b *Buffer) Pop() int {
-	if b.IsEmpty() {
-		panic("Cannot pop empty Buffer")
+func (b *Buffer) Pop() (front int) {
+	if b.Empty() {
+		panic("ring.Buffer.Pop: cannot pop an empty Buffer")
 	}
 
-	top := b.Data[b.Front]
-	b.Data[b.Front] = b.Default
-
-	if b.Front == b.Back {
-		b.Empty = true
-	} else {
-		b.Front = (b.Front + len(b.Data) - 1) % len(b.Data)
+	front = b.data[b.front]
+	if !b.Empty() {
+		b.front = (b.front + len(b.data) - 1) % len(b.data)
 	}
+	return front
+}
 
-	return top
+// Peek returns the first element of the Buffer without
+// removing it.
+func (b *Buffer) Peek() (front int) {
+	if b.Empty() {
+		panic("ring.Buffer.Peek: cannot pop an empty Buffer")
+	}
+	return b.data[b.front]
 }
